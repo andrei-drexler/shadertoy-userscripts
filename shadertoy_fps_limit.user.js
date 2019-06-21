19bae7e43911cf5abf4fc9f3aca7ed50ef9497da
Andrei Drexler <andreidrx-github@yahoo.com>
2019-06-21 22:19:43 +0300

    Create shadertoy_fps_limit.user.js

---
+++ shadertoy_fps_limit.user.js
@@ -0,0 +1,113 @@
+// ==UserScript==
+// @name         Shadertoy FPS limit
+// @namespace    http://tampermonkey.net/
+// @version      0.1.20190621
+// @description  Configurable framerate limit
+// @author       Andrei Drexler
+// @match        https://www.shadertoy.com/view/*
+// @match        https://www.shadertoy.com/new
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+/* global Effect */
+
+(function() {
+    'use strict';
+
+    const PREDEFINED_LIMITS = [30, 48, 60, 72, 90, 100, 120, 144];
+    const USE_PREDEFINED_LIMITS = false;
+
+    let useLimit = false,
+        targetFPS = 30,
+        playerBar = document.getElementById("playerBar");
+
+    if (playerBar) {
+        let toggleLimit = document.createElement("a");
+        toggleLimit.classList.add("playerBarText");
+        toggleLimit.classList.add("regular");
+        toggleLimit.style = "cursor:pointer;position:absolute;left:240px;top:2px;bottom:2px;width:21px;text-align:center;";
+        toggleLimit.id = "myCustomFPSLimitToggle";
+        playerBar.appendChild(toggleLimit);
+
+        let limitInput = document.createElement("input");
+        const INPUT_WIDTH = USE_PREDEFINED_LIMITS ? 68 : 48;
+        limitInput.type = "number";
+        limitInput.classList.add("inputForm");
+        limitInput.style = "position:absolute;left:264px;top:2px;bottom:2px;width:0px;visibility:hidden;padding-left:2px;padding-right:initial;transition:width 0.125s;";
+        limitInput.placeholder = "Limit";
+        limitInput.min = 1;
+        limitInput.value = targetFPS;
+        playerBar.appendChild(limitInput);
+
+        if (USE_PREDEFINED_LIMITS) {
+            let limitValues = document.createElement("datalist");
+            limitValues.id = "myFPSLimitValues";
+            for (let limit of PREDEFINED_LIMITS) {
+                let option = document.createElement("option");
+                option.value = limit;
+                limitValues.appendChild(option);
+            }
+            playerBar.appendChild(limitValues);
+            limitInput.setAttribute("list", limitValues.id);
+        }
+
+        limitInput.onchange = function(ev) {
+            targetFPS = ev.target.value;
+        };
+
+        let canvas = document.getElementById("demogl");
+        function updateStatus() {
+            if (useLimit) {
+                toggleLimit.text = "\u2243";
+                toggleLimit.title = "Click to disable FPS limit";
+                limitInput.style.width = INPUT_WIDTH + "px";
+                limitInput.style.visibility = "visible";
+            } else {
+                toggleLimit.text = "\u221e";
+                toggleLimit.title = "Click to enable FPS limit (" + targetFPS + ")";
+                limitInput.style.width = "0px";
+                window.setTimeout(function() {
+                    if (!useLimit) {
+                        limitInput.style.visibility = "hidden";
+                    }
+                }, 100);
+            }
+            if (canvas) {
+                canvas.focus();
+            }
+        }
+
+        updateStatus();
+        toggleLimit.onclick = function(ev) {
+            useLimit = !useLimit;
+            updateStatus();
+        };
+    }
+
+    let nextTime = performance.now();
+    let oldRAF = Effect.prototype.RequestAnimationFrame;
+    Effect.prototype.RequestAnimationFrame = function(id) {
+        let effect = this;
+        function throttle(timestamp) {
+            if (!useLimit) {
+                id(timestamp);
+                nextTime = timestamp;
+                return;
+            }
+            let targetMsec = 1000 / Math.max(targetFPS, 1);
+            let delta = timestamp - nextTime;
+            if (delta < -0.5 * targetMsec) {
+                oldRAF.call(effect, throttle);
+                return;
+            }
+            if (delta <= 0) {
+                nextTime += targetMsec;
+            } else {
+                nextTime = timestamp + targetMsec - delta % targetMsec;
+            }
+            id(timestamp);
+        }
+        oldRAF.call(this, throttle);
+    };
+})();
